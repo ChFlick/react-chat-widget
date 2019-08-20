8eeb9e9473b28a7cfcd9b63ed504804c35ea50f2
Christoph Flick <christophflick@gmx.de>
2019-08-20 17:01:55 +0200

    inline inject stlyes

--- assets/inject.js
+++ assets/inject.js
@@ -26,8 +26,32 @@ window.addEventListener('message', function (payload) {
 });
 
 function init(host) {
-    const cssHref = './inject.css';
-    injectDOMElement('link', 'head', { rel: 'stylesheet', href: cssHref });
+    const styles = document.createElement('style');
+    styles.innerHTML = `
+    .rcw-widget-container {
+        bottom: 0;
+        display: flex;
+        flex-direction: column;
+        margin: 0 20px 20px 0;
+        max-width: 370px;
+        position: fixed;
+        right: 0;
+        z-index: 9999;
+    }
+    
+    .rcw-opened {
+        width: 90vw;
+    }
+    
+    .rcw-launcher {
+        align-self: flex-end;
+        border: 0;
+        border-radius: 50%;
+        height: 100px;
+        margin-top: 10px;
+        width: 100px;
+    }`;
+    document.head.appendChild(styles);
 
     const iframeHTML = '<iframe width="1000" height="1000" id="chat-widget" frameborder="0" src="' + host + '/" class="rcw-launcher"/>';
     injectDOMElement('div', 'body', { id: 'chat-widget-frame', innerHTML: iframeHTML, className: 'rcw-widget-container' });
